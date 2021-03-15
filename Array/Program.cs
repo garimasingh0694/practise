56f14f10ae9a9c12ca5f6e60bdd24b82500a0a85
garimasingh0694 <80271758+garimasingh0694@users.noreply.github.com>
2021-03-15 10:46:34 -0700

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+            */
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+            String[] names = new string[] { "Eddie", "Alex", "Michael", "DAvid Lee" };
+
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+           
+            
+           
+
+           
+
+
+            
+
+        }
+    }
+}
