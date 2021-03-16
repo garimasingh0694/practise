f1f6c66504a2d1b4a6bebf880faf73693b2ad6ab
garimasingh0694 <80271758+garimasingh0694@users.noreply.github.com>
2021-03-16 11:08:08 -0700

    Add files via upload

---
+++ SimpleClasses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "OldsMobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:c}", value);
+            Console.WriteLine("{0:c}", myCar.DetermineMarketValue());
+
+            Console.ReadLine();
+        }
+        private static decimal DeterminemarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+
+
+            return carValue;
+        }
+
+    } 
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }  
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 20000;
+
+            return carValue;
+        }
+
+       
+    }
+}
